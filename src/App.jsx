04b0859fc7f62c5f3e5448558f975e46fe1764d0
100ngramjit <sangramjitdutta@gmail.com>
2023-03-29 19:10:04 +0530

    aircampus logo implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import "./App.css";
 import Header from "./components/Header";
 
+const catagories = [
+  "Popular",
+  "Data Science",
+  "Technology",
+  "Management",
+  "Arts",
+];
 function App() {
   return (
     <>
       <Header />
-      xyz
+      <Typography variant="h2">High-growth programs</Typography>
+      <Typography variant="p">
+        We design and deliver industry-relevant programs with access to top
+        global universities, to create the change makers of tomorrow
+      </Typography>
+      <div>
+        {catagories?.map((ele) => (
+          <Button
+            key={ele}
+            variant="outlined"
+            color="inherit"
+            sx={{ m: 2, p: 2 }}
+          >
+            {ele}
+          </Button>
+        ))}
+      </div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,22 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
+import airCampus_logo from "../assets/logoaircampus.png";
 import React from "react";
 
 const Header = () => {
   return (
     <AppBar color="secondary" component="nav" sx={{ minHeight: "12vh" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Box>
-          AirCampus{" "}
-          <Button variant="contained" size="large">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <img
+            src={airCampus_logo}
+            style={{ height: "65px", width: "200px", paddingTop: "20px" }}
+          />
+          <Button variant="contained" size="large" sx={{ m: 3 }}>
             Programs
           </Button>
-        </Box>
+        </div>
         <Box>
-          <Button color="secondary" variant="contained" size="large">
+          <Button color="inherit" variant="outlined" size="large">
             Start Learning
           </Button>
         </Box>
