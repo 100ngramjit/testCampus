9254880ed565a729d2d35ccd52afd74beef69d71
Souvikdeb2612 <souvikdeb26@gmail.com>
2023-03-31 23:16:31 +0530

    fix ui

--- src/components/PlacementsCarousal.jsx
+++ src/components/PlacementsCarousal.jsx
@@ -5,28 +5,38 @@ import * as images from "../assets/placements/images";
 function ImageGallery() {
   console.log(images);
   return (
-    <Carousel
-      showIndicators={false}
-      infiniteLoop={true}
-      autoPlay={true}
-      centerMode={true}
-      centerSlidePercentage="25"
-      showThumbs={false}
-      interval={1000}
-      transitionTime={300}
-      swipeable={true}
-      emulateTouch={true}
-      animation="slide"
-    >
-      {Object.entries(images).map(([key, value]) => (
-        <img
-          key={key}
-          src={value}
-          alt="gallery"
-          style={{ height: 75, width: 150 }}
-        />
-      ))}
-    </Carousel>
+    <>
+      <div className="text-center mb-5">
+        <h3 className="section_header">Where our learners work</h3>
+        <p>
+          Programs with built-in career and placement assistance for our
+          learners to find success and growth
+        </p>
+      </div>
+
+      <Carousel
+        showIndicators={false}
+        infiniteLoop={true}
+        autoPlay={true}
+        centerMode={true}
+        centerSlidePercentage="20"
+        showThumbs={false}
+        interval={2000}
+        transitionTime={2000}
+        swipeable={true}
+        emulateTouch={true}
+        animation="slide"
+      >
+        {Object.entries(images).map(([key, value]) => (
+          <img
+            key={key}
+            src={value}
+            alt="gallery"
+            style={{ height: 75, width: 150 }}
+          />
+        ))}
+      </Carousel>
+    </>
   );
 }
 
