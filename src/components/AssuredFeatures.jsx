92eb2bc33b3482603d47b09d7c932ac10ed321d4
100ngramjit <sangramjitdutta@gmail.com>
2023-04-01 15:26:57 +0530

    Assured Features section added

---
+++ src/components/AssuredFeatures.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AssuredFeatures = () => {
+  return (
+    <div>
+      <div className="text-center mb-5">
+        <h2 className="section_header">
+          At AirCampus, Your Earnings Don’t Have To Wait Till Graduation
+        </h2>
+      </div>
+      <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        <strong>Assured Internships</strong> at the end of 1st year at leading
+        global tech companies & emerging startups
+      </div>
+      <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        Earn upto <strong> ₹ 8 Lakhs</strong> in stipends
+      </div>
+      <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        Attract <strong>3X salary</strong> and growth upon graduation in
+        comparison with other graduates
+      </div>
+    </div>
+  );
+};
+
+export default AssuredFeatures;
